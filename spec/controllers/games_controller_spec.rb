b5d6bb45af0f65e26171ff37b65610b6d8de8499
Andy Strube <andystrube64@gmail.com>
2020-09-03 18:51:03 -0700

    Added some tests for the games controller

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  describe "games#new action" do
+    it "should authenticate the user" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
 
+    it "should load the page" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
