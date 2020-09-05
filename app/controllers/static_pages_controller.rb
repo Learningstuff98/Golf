7c88d992a8dbdb01635fd68756c556aac287939b
Andy Strube <andystrube64@gmail.com>
2020-09-04 17:12:28 -0700

    Added a lobby page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!, only: [:lobby]
 
   def index
+  end
+
+  def lobby
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  get 'lobby', to: 'static_pages#lobby'
   resources :games, only: [:new, :create, :show, :destroy]
 end
